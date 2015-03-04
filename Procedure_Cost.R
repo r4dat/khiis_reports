83dd35f350d3febe0dfbfba154b0676b591319fd
“r4dat <rfourtoss@gmail.com>
2015-03-04 13:17:17 -0600

    Whoops. Put year_string parameter twice, mean procedure.

--- Procedure_Cost.R
+++ Procedure_Cost.R
@@ -11,21 +11,21 @@ options("scipen"=10)
 
 if(!exists("readProcedureData", mode="function")) source("readData.R")
 
-colo10 = readProcedureData(year_string = 2010,year_string = "Colonoscopy")
-colo11 = readProcedureData(year_string = 2011,year_string = "Colonoscopy")
-colo12 = readProcedureData(year_string = 2012,year_string = "Colonoscopy")
+colo10 = readProcedureData(year_string = 2010,procedure =  "Colonoscopy")
+colo11 = readProcedureData(year_string = 2011,procedure = "Colonoscopy")
+colo12 = readProcedureData(year_string = 2012,procedure = "Colonoscopy")
 
-hip10 = readProcedureData(year_string = 2010,year_string = "Hip")
-hip11 = readProcedureData(year_string = 2010,year_string = "Hip")
-hip12 = readProcedureData(year_string = 2010,year_string = "Hip")
+hip10 = readProcedureData(year_string = 2010,procedure = "Hip")
+hip11 = readProcedureData(year_string = 2011,procedure = "Hip")
+hip12 = readProcedureData(year_string = 2012,procedure = "Hip")
 
-gall10 = readProcedureData(year_string = 2010,year_string = "Gallbladder")
-gall11 = readProcedureData(year_string = 2010,year_string = "Gallbladder")
-gall12 = readProcedureData(year_string = 2010,year_string = "Gallbladder")
+gall10 = readProcedureData(year_string = 2010,procedure = "Gallbladder")
+gall11 = readProcedureData(year_string = 2011,procedure = "Gallbladder")
+gall12 = readProcedureData(year_string = 2012,procedure = "Gallbladder")
 
-mammo10 = readProcedureData(year_string = 2010,year_string = "Mammo")
-mammo11 = readProcedureData(year_string = 2010,year_string = "Mammo")
-mammo12 = readProcedureData(year_string = 2010,year_string = "Mammo")
+mammo10 = readProcedureData(year_string = 2010,procedure = "Mammo")
+mammo11 = readProcedureData(year_string = 2011,procedure = "Mammo")
+mammo12 = readProcedureData(year_string = 2012,procedure = "Mammo")
 
 colo = rbind(colo10,colo11,colo12)
 hip = rbind(hip10,hip11,hip12)
