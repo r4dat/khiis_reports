c6a3eb3bfed4a7482ea922dc5be00d5bdfaebe69
“r4dat <rfourtoss@gmail.com>
2015-03-04 13:15:04 -0600

    Add file read-ins, and bind to single dataframe per procedure.

--- Procedure_Cost.R
+++ Procedure_Cost.R
@@ -7,5 +7,27 @@ library(assertthat)
 
 options("scipen"=10)
 
+## County, State, Year
+
 if(!exists("readProcedureData", mode="function")) source("readData.R")
 
+colo10 = readProcedureData(year_string = 2010,year_string = "Colonoscopy")
+colo11 = readProcedureData(year_string = 2011,year_string = "Colonoscopy")
+colo12 = readProcedureData(year_string = 2012,year_string = "Colonoscopy")
+
+hip10 = readProcedureData(year_string = 2010,year_string = "Hip")
+hip11 = readProcedureData(year_string = 2010,year_string = "Hip")
+hip12 = readProcedureData(year_string = 2010,year_string = "Hip")
+
+gall10 = readProcedureData(year_string = 2010,year_string = "Gallbladder")
+gall11 = readProcedureData(year_string = 2010,year_string = "Gallbladder")
+gall12 = readProcedureData(year_string = 2010,year_string = "Gallbladder")
+
+mammo10 = readProcedureData(year_string = 2010,year_string = "Mammo")
+mammo11 = readProcedureData(year_string = 2010,year_string = "Mammo")
+mammo12 = readProcedureData(year_string = 2010,year_string = "Mammo")
+
+colo = rbind(colo10,colo11,colo12)
+hip = rbind(hip10,hip11,hip12)
+gall = rbind(gall10,gall11,gall12)
+mammo = rbind(mammo10,mammo11,mammo12)
